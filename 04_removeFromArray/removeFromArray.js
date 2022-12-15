c0c74c9290dfa59b9b5036f18f1c6f824611d9f2
WCuypers <wannes.cuypers1@gmail.com>
2022-12-15 14:15:19 +0100

    Replace default arguments for arguments array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,7 @@
-const removeFromArray = function(array, value) {
-    let indexValue = array.indexOf(value); // find the index of value in array
-    valueFromArray = array.splice(indexValue, 1); // cut out the array item in the place of index
-    return array; // return the new array
+const removeFromArray = function() {
+    let indexValue = arguments[0].indexOf(arguments[1]); // find the index of arguments[1] in array
+    valueFromArray = arguments[0].splice(indexValue, 1); // cut out the array item in the place of index
+    return arguments[0]; // return the new array
 };
 
 // Do not edit below this line

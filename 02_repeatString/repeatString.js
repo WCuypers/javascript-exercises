b4cb1b2d337264a4dff7cd9542c1f0445b554338
WCuypers <wannes.cuypers1@gmail.com>
2022-12-15 11:07:53 +0100

    Clarify code with comments.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,12 +1,14 @@
 const repeatString = function(string, num) {
-    if (num == 0) {
-        return "";
-    } else {
-        let repeatedString = string;
-        for (let i = 1; i < num; i++) {
+    if (num == 0) { //if num == 0
+        return ""; //return an empty string (0 * a word = nothing)
+    } else if (num < 0) { //if num is negative
+        return "ERROR"; //return error
+    } else { //if num is positive carry out the repeat function
+        let repeatedString = string; //value to be printed is string once
+        for (let i = 1; i < num; i++) { //add string again for each integer between 1 and num
             repeatedString += string;
         }
-        return repeatedString;
+        return repeatedString; //print value
     }
 };
 

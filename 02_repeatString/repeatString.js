aa246d1f9190763b713ff42c98fa24b32f13aa21
WCuypers <wannes.cuypers1@gmail.com>
2022-12-15 11:03:13 +0100

    Edit function to output "" if num is 0

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,13 @@
 const repeatString = function(string, num) {
-    let repeatedString = string;
-    for (let i = 1; i < num; i++) {
-        repeatedString += string;
+    if (num == 0) {
+        return "";
+    } else {
+        let repeatedString = string;
+        for (let i = 1; i < num; i++) {
+            repeatedString += string;
+        }
+        return repeatedString;
     }
-    return repeatedString;
 };
 
 // Do not edit below this line

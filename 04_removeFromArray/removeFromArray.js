92f568b74eebd0abd6e3787fad09f008a02c2d2a
WCuypers <wannes.cuypers1@gmail.com>
2022-12-15 14:06:42 +0100

    Write initial function.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, value) {
+    let indexValue = array.indexOf(value); // find the index of value in array
+    valueFromArray = array.splice(indexValue, 1); // cut out the array item in the place of index
+    return array; // return the new array
 };
 
 // Do not edit below this line

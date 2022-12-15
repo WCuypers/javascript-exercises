d500b41b7c4ef2526f7ea0b40ffb75b34529b549
WCuypers <wannes.cuypers1@gmail.com>
2022-12-15 10:53:41 +0100

    Add html for console and add initial function.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function("hey", 3) {
+    let repeatedString
+    for (let i = 1; i <= num; i++) {
+        repeatedString += string;
+        console.log(repeatedString)
+    }
 };
 
 // Do not edit below this line

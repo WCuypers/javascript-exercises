aa9ed4abc191cda60f7d3dcfc10558ce27e5ab8e
WCuypers <wannes.cuypers1@gmail.com>
2022-12-15 13:51:34 +0100

    Try initial function.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringArray = string.split("");
+    let reversedArray = stringArray.reverse();
+    let reversedString = reversedArray.join("");
+    console.log(reversedString);
 };
 
 // Do not edit below this line

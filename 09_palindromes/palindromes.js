c259debe5e2aaa23ed1e2af089bc943487838700
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 19:10:15 +0100

    Write initial function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/\s+/g, ""); //removes spaces from string
+    string = string.replace(/\,+/g, ""); //remove commas from string
+    for (let i = 1; i < string.length / 2; i++) {
+        if (string.slice(i, 1) == string.slice(-i, 1)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
 // Do not edit below this line

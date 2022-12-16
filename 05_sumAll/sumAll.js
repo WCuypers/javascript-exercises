b149ca0a8e34b51675cf16496cf87a6c130ce7a2
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 13:40:54 +0100

    Add input type check.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,6 +2,8 @@ const sumAll = function(num1, num2) {
     let result =0;
     if (num1 < 0 || num2 < 0) {
         return "ERROR";
+    } else if (typeof(num1) !== "number" || typeof(num2) !== "number"){
+        return "ERROR";
     } else {
         if (num1 < num2) {
             for (let i = num1; i <= num2; i++) {

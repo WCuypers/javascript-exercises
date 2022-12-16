0b587f1f7c75e666216c836f727527827d74b57b
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 19:14:08 +0100

    Create index.html for debugging

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,9 @@
 const palindromes = function (string) {
     string = string.replace(/\s+/g, ""); //removes spaces from string
     string = string.replace(/\,+/g, ""); //remove commas from string
+    string = string.toLowerCase();
     for (let i = 1; i < string.length / 2; i++) {
-        if (string.slice(i, 1) == string.slice(-i, 1)) {
+        if (string.slice(i, 1) === string.slice(-i, 1)) {
             return true;
         } else {
             return false;

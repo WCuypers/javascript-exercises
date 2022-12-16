14d91ab854da979f689085c1acb8ef43cbe1df2f
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 13:36:23 +0100

    Return "ERROR" if negative number.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,16 +1,20 @@
 const sumAll = function(num1, num2) {
     let result =0;
-    if (num1 < num2) {
-        for (let i = num1; i <= num2; i++) {
-            result += i;
-        }        
-    } else if (num1 > num2) {
-        for (let i = num2; i<= num1; i++) {
-            result += i;
-        }
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
     } else {
-        result = num1 = num2;
-    };
+        if (num1 < num2) {
+            for (let i = num1; i <= num2; i++) {
+                result += i;
+            }        
+        } else if (num1 > num2) {
+            for (let i = num2; i<= num1; i++) {
+                result += i;
+            }
+        } else {
+            result = num1 = num2;
+        };
+    }
     return result;
 };
 

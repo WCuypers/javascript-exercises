812925e33e9aed3f446d43a1ff3523b7bd794929
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 15:12:48 +0100

    Set up initial function.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+// if year % 4 === 0
+// if year % 100 != 0 OR year % 400 == 0
+// ^CHANGE ORDER OF THESE STATEMENTS (denk %100 eerst?)
+    if(year % 100 == 0 && year % 400 != 0) {
+        return false;
+    } else if(year % 4 == 0) {
+        return true;
+    } else {
+        return false;
+    };
 };
 
 // Do not edit below this line

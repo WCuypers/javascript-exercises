d40cd9d87b332115a0ab33d81e5da04c57a8711f
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 13:34:13 +0100

    Debug else condition on row 7.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,10 @@
 const sumAll = function(num1, num2) {
-    let result = 0;
+    let result =0;
     if (num1 < num2) {
         for (let i = num1; i <= num2; i++) {
             result += i;
         }        
-    } else if (num2 > num1) {
+    } else if (num1 > num2) {
         for (let i = num2; i<= num1; i++) {
             result += i;
         }

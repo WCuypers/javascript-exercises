55e1834ace9e0500b3efba777e7b64db52ff0ae8
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 20:58:56 +0100

    Make edits for more advanced tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,11 +1,16 @@
 const fibonacci = function(index) {
-    let n1 = 0, n2 = 1, nextNumber;
-    for (let i = 1; i <= index; i++) {
-        nextNumber = n1 + n2;
-        n1 = n2;
-        n2 = nextNumber;
+    index = +index;
+    if (index < 0) {
+        return "OOPS"
+    } else {
+        let n1 = 0, n2 = 1, nextNumber;
+        for (let i = 1; i <= index; i++) {
+            nextNumber = n1 + n2;
+            n1 = n2;
+            n2 = nextNumber;
+        }
+        return n1;
     }
-    return n1;
 };
 
 // Do not edit below this line

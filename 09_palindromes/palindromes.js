5cab6185b4a26115c987caf376063a6ad6ddadc1
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 20:38:47 +0100

    Debug

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,21 @@
-const palindromes = function (string) {
+const palindromes = function(string) {
     string = string.replace(/\s+/g, ""); //removes spaces from string
-    string = string.replace(/\,+/g, ""); //remove commas from string
-    string = string.toLowerCase();
-    for (let i = 1; i < string.length / 2; i++) {
-        if (string.slice(i, 1) === string.slice(-i, 1)) {
-            return true;
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"") //remove commas from string
+    string = string.toLowerCase(); //put string in lower case
+    halfLength = Math.floor(string.length / 2); //halves string length, round down
+    for (let i = 0; i < halfLength; i++) {
+        if (string.charAt(i) === string.charAt(string.length-1-i)) {
+            console.log(string.charAt(i));
+            console.log(string.charAt(string.length-1-i));
+            result = true;
         } else {
+            //console.log(string.charAt(i));
+            //console.log(string.charAt(string.length-1-i)); //this one does not return the 2nd letter
             return false;
-        }
-    }
+            break;
+        };
+    };
+    return result
 };
 
 // Do not edit below this line

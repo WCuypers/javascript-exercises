a28348a388be3474a17ffc8c3e336a4a0886fb90
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 13:09:03 +0100

    Filter out non present values.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -18,17 +18,21 @@ console.log(removeFromArray([1, 2, 3, 4], 2)); //only logs 2 values, not 3
 const removeFromArray = function() {
     const itemsToRemove = [];
     let originalArray = arguments[0];
-    for (let i = 1; i < arguments.length; i++) { //makes a new array with all items to remove
-        itemsToRemove.push(arguments[i]);
-    };
-    //!!write a loops that finds every item of itemsToRemove, finds its place in originalArray and removes it
-    for (const item of itemsToRemove) {
-    // for each value of itemsToRemove, find its index in originalArray
-    let indexValue = originalArray.indexOf(item);
-    let valueFromArray = originalArray.splice (indexValue, 1);
-    // use the index in originalArray to remove the item
-    };
-    return originalArray;
+        for (let i = 1; i < arguments.length; i++) { //makes a new array with all items to remove
+            if (originalArray.includes(arguments[i])) {
+                itemsToRemove.push(arguments[i]);
+            } else {
+                continue;
+            }
+        };
+        //!!write a loops that finds every item of itemsToRemove, finds its place in originalArray and removes it
+        for (const item of itemsToRemove) {
+        // for each value of itemsToRemove, find its index in originalArray
+        let indexValue = originalArray.indexOf(item);
+        let valueFromArray = originalArray.splice (indexValue, 1);
+        // use the index in originalArray to remove the item
+        };
+        return originalArray;
 };
 
 // V console testing

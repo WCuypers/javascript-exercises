9388044334db9c9eabf3432f8dedca48e3efbf90
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 15:18:01 +0100

    Set up initial functions.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius = fahrenheit * (9/5) + 32;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(fahrenheit) {
+  fahrenheit = (celsius - 32) * (5/9);
 };
 
 // Do not edit below this line

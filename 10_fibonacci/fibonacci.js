f94e30f836c949a9b49b9b920e292310d57ab23b
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 20:46:34 +0100

    Set up initial code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    result = 1;
+    for (let i = 1; i <= index; i++) {
+        result += result;
+        console.log(result);
+    }
+    return result;
 };
 
 // Do not edit below this line

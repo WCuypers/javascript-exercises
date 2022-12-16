5380e3415eab8bd80823d67b3c5a9aee99aa75a0
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 15:24:04 +0100

    Edit round to one decimal

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,11 @@
 const convertToCelsius = function(fahrenheit) {
   celsius = (5 / 9) * (fahrenheit - 32);
-  return Math.round(celsius);
+  return Math.round(celsius * 10) / 10;
 };
 
 const convertToFahrenheit = function(fahrenheit) {
   fahrenheit = celsius * 1.8 + 32;
-  return Math.round(fahrenheit);
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line

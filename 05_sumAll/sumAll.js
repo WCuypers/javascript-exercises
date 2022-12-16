1665164cdb65bff0a37a725305d3da30962e3e80
WCuypers <wannes.cuypers1@gmail.com>
2022-12-16 13:30:42 +0100

    Set up initial formula.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let result;
+    if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            result += i;
+        }        
+    } else if (num2 > num1) {
+        for (let i = num2; i<= num1; i++) {
+            result += i;
+        }
+    } else {
+        result = num1 = num2;
+    };
+    return result;
 };
 
 // Do not edit below this line

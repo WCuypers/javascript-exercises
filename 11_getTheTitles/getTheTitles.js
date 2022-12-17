52b46f73aff00109d5a3e42ecabfa042578106a8
WCuypers <wannes.cuypers1@gmail.com>
2022-12-17 17:44:38 +0100

    Do small layout edit

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,6 @@
 const getTheTitles = function(bookArray) {
     let titleArray = [];
-    for (let i=0; i < bookArray.length; i++){
+    for (let i = 0; i < bookArray.length; i++){
     let item = bookArray[i];
     let test = item.title;
     titleArray.push(test);

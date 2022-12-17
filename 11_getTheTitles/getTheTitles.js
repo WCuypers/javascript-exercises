a7b954dba7c3db168002c1f92eb27b2f90357d21
WCuypers <wannes.cuypers1@gmail.com>
2022-12-17 17:43:53 +0100

    Rewrite initial code.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
-};
+const getTheTitles = function(bookArray) {
+    let titleArray = [];
+    for (let i=0; i < bookArray.length; i++){
+    let item = bookArray[i];
+    let test = item.title;
+    titleArray.push(test);
+    }
+    return(titleArray);
+    };
 
 // Do not edit below this line
 module.exports = getTheTitles;

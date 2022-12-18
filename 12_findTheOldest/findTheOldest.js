4bd19fa692f45f73f99379b95ffad682b88658b3
WCuypers <wannes.cuypers1@gmail.com>
2022-12-18 11:33:43 +0100

    Fixing the code.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,13 +1,37 @@
-const findTheOldest = function(peopleArray) {
+const findTheOldest = function(array) {
     const currentYear = 2022;
     let age;
-    for (i = 0 ; i < peopleArray.length; i++) {    // for each element of the array
-        age = yearOfDeath - yearOfBirth;
-        return age
-            // store result in age variable    
+    let oldest;
+    for (i = 0 ; i < array.length; i++) {    // for each element of the array
+        array[i].age = array[i].yearOfDeath - array[i].yearOfBirth; // store result in age variable
+        if (i > 0) {
+            oldest = Math.max(array[i].age, array[i-1].age);
+        } else {
+            continue;
+        }
     }
-    // return name linked to highest age variable
+    let arrayIndex = array.find(item => item.age === oldest);
+    return arrayIndex;    // return name linked to highest age variable
 };
 
+/*//FOR TESTING
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+console.log(findTheOldest(people));*/
 // Do not edit below this line
 module.exports = findTheOldest;

33dc5d4d8ecf4f51286bd40720697e464e234c5c
WCuypers <wannes.cuypers1@gmail.com>
2022-12-18 11:06:59 +0100

    Setup html for debugging in devtools.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArray) {
+    const currentYear = 2022;
+    let age;
+    for (i = 0 ; i < peopleArray.length; i++) {    // for each element of the array
+        age = yearOfDeath - yearOfBirth;
+        return age
+            // store result in age variable    
+    }
+    // return name linked to highest age variable
 };
 
 // Do not edit below this line

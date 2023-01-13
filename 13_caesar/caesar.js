e880f87907347c88fb0f87baf47b139533d997db
WCuypers <wannes.cuypers1@gmail.com>
2023-01-13 15:44:30 +0100

    Debug code by using .fromCharcode correctly.

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,10 +1,10 @@
 const caesar = function(string, number) {
-    let stringArray = string.split('');                         //Turn string into array
+    let stringArray = string.split("");                         //Turn string into array
     for (let i = 0; i < stringArray.length; i++) {              //For every item of the array(= letter of the word)
         stringArray[i] = string.charCodeAt(stringArray[i]);         //Turn letter into unicode
         stringArray[i] += number;                                   //Do unicode - number to apply the transition
     };
-    return stringArray;
+    return String.fromCharCode(stringArray);
 };
 
 // Do not edit below this line

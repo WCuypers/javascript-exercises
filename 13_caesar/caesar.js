e4501ea38a133ee24ee42812d13b321516fe41a9
WCuypers <wannes.cuypers1@gmail.com>
2023-01-14 11:23:37 +0100

    Edit comments.

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,7 +1,7 @@
 const caesar = function(string, number) {
     let stringArray = string.split("");                         //Turn string into array
     for (let i = 0; i < stringArray.length; i++) {              //For every item of the array(= letter of the word)
-        stringArray[i] = stringArray[i].charCodeAt(0);         //Turn letter into unicode -> turns lowercase and uppercase into same unicode value
+        stringArray[i] = stringArray[i].charCodeAt(0);         //Turn letter into unicode
         stringArray[i] += number;                                   //Do unicode - number to apply the transition
         stringArray[i] = String.fromCharCode(stringArray[i]);       //Turn new unicode into letter
     };

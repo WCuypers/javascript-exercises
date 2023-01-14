b15f74ea553b6966744ff543b7d92b185e7c4090
WCuypers <wannes.cuypers1@gmail.com>
2023-01-14 11:23:14 +0100

    Debug letter to unicode (line 4).

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,10 +1,11 @@
 const caesar = function(string, number) {
     let stringArray = string.split("");                         //Turn string into array
     for (let i = 0; i < stringArray.length; i++) {              //For every item of the array(= letter of the word)
-        stringArray[i] = string.charCodeAt(stringArray[i]);         //Turn letter into unicode
+        stringArray[i] = stringArray[i].charCodeAt(0);         //Turn letter into unicode -> turns lowercase and uppercase into same unicode value
         stringArray[i] += number;                                   //Do unicode - number to apply the transition
+        stringArray[i] = String.fromCharCode(stringArray[i]);       //Turn new unicode into letter
     };
-    return String.fromCharCode(stringArray);
+    return stringArray.join("");                                //Return string made from array.
 };
 
 // Do not edit below this line
